acedbbffeb91e499374cd6c3d3cd767ee46e7ee1
Loukas Litsos <lkslts64@gmail.com>
2019-07-19 16:53:39 +0300

    Chaincode basic functionality has been tested. work with accounts now

--- first-network/mychaincode_copy/supply_chain/supply_chain_CC/threeOrgs2/threeOrgs/all-orgsCC.go
+++ first-network/mychaincode_copy/supply_chain/supply_chain_CC/threeOrgs2/threeOrgs/all-orgsCC.go
@@ -1,7 +1,7 @@
-//convert all times in time.Parse(rfc3999,arg_given)
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -13,13 +13,24 @@ import (
 )
 
 /*
+
+org1 -> driller
+org2 -> shipper
+org3 -> refiner
+org4 -> distributor
+org5/6 -> retailer / fuel stations
+
+
+
 API:
 
-addCrude
+deliverCrude
 refine
-addFuelForDelivery - coupled with a retailer.
+addFuelOrder - coupled with a retailer.
 deliverFuel - make a plan for distributing to different retailers. accumulate addFuelDelivery tx's.
-
+transfer - either crude or fuel
+query asset
+query asset by range
 
 */
 // Define the Smart Contract structure
@@ -35,7 +46,6 @@ type Vehicle struct {
 	ID   string
 }
 type DeliveryDetails struct {
-	//State            string
 	EstTime          time.Time
 	Delay            float64
 	StartingLocation string
@@ -93,6 +103,7 @@ type FuelOrder struct {
 type FuelOrderID = string
 
 /*
+ID form : 'PlanXXXX'
 A delivery plan from refinary towards the gas stations.
 Contains the vehicle that will deliver the fuels at many fueling stations
 A map for easy access to delivery details with key the orders that org2 has added.
@@ -101,11 +112,6 @@ type FuelDeliveryPlan struct {
 	Veh  Vehicle
 	Plan map[FuelOrderID]DeliveryDetails
 }
-
-/*
-CrudeOil becomes Fuel at refinary. A tx should be submited in order to make this transformation.
-Produced by org2 - at refinary and distributed by org3
-*/
 
 /*
 * The Init method *
@@ -130,6 +136,16 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 		return s.refine(APIstub, args)
 	} else if function == "addFuelOrder" {
 		return s.addFuelOrder(APIstub, args)
+	} else if function == "deliverFuel" {
+		return s.deliverFuel(APIstub, args)
+	} else if function == "transfer" {
+		return s.transfer(APIstub, args)
+	} else if function == "queryAsset" {
+		return s.queryAsset(APIstub, args)
+	} else if function == "queryAssetByRange" {
+		return s.queryAssetByRange(APIstub, args)
+	} else if function == "initLedger" {
+		return s.initLedger(APIstub, args)
 	}
 
 	return shim.Error("Invalid Smart Contract function name.")
@@ -190,6 +206,7 @@ func RFCtoTime(rfc string) (time.Time, error) {
 args[0] = crudeID like 'CrudeXXXX'
 arg1 = value,arg2 = quantity, arg3 = owner
 arg4 = estTime, arg5 = startLoc, arg6 = dest
+arg7 = vesselID , arg8 = timestamp
 */
 func (s *SmartContract) deliverCrude(stub shim.ChaincodeStubInterface, args []string) sc.Response {
 	//check if creator is org1-shipper??
@@ -204,6 +221,11 @@ func (s *SmartContract) deliverCrude(stub shim.ChaincodeStubInterface, args []st
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	crudeAsBytes, _ := stub.GetState(args[0])
+	if crudeAsBytes != nil {
+		return shim.Error(fmt.Sprintf("Crude with id %s already exists", args[0]))
+	}
+
 	Proof := NewProof()
 	//hardcoded vehID.TODO: construct base on the Hash(args[1]+args[2]...+)
 	Veh := NewVehicle("Vessel", args[7])
@@ -212,8 +234,7 @@ func (s *SmartContract) deliverCrude(stub shim.ChaincodeStubInterface, args []st
 		return shim.Error(err.Error())
 	}
 	crude := Crude{AD, DD, Proof, Veh, Timestamp}
-	crudeAsBytes, _ := json.Marshal(crude)
-	//TODO: check if crude already exists.
+	crudeAsBytes, _ = json.Marshal(crude)
 	err = stub.PutState(args[0], crudeAsBytes) //if an crude with the same ID already exists, this will override it.
 	if err != nil {
 		return shim.Error(fmt.Sprintf("Failed to add crude: %s", args[0]))
@@ -279,8 +300,8 @@ func (s *SmartContract) addFuelOrder(stub shim.ChaincodeStubInterface, args []st
 	}
 	Proof := NewProof()
 	//check that fuelID exists
-	fuelbytes, _ := stub.GetState(args[5])
-	if fuelbytes == nil {
+
+	if fuelbytes, _ := stub.GetState(args[5]); fuelbytes == nil {
 		return shim.Error("FuelID doens't exist!")
 	}
 	Timestamp, err := RFCtoTime(args[6])
@@ -288,7 +309,7 @@ func (s *SmartContract) addFuelOrder(stub shim.ChaincodeStubInterface, args []st
 		return shim.Error(err.Error())
 	}
 	//check that fuelOrderID doens't exist
-	if fuelOrderbytes, _ := stub.GetState(args[5]); fuelOrderbytes != nil {
+	if fuelOrderbytes, _ := stub.GetState(args[1]); fuelOrderbytes != nil {
 		return shim.Error("FuelOrderID already exists")
 	}
 
@@ -314,39 +335,39 @@ args of this invokation:
 	{FuelOrderID,EstTime,Sloc,Dest}
 */
 func (s *SmartContract) deliverFuel(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+	//check that client supplied properly the # of args
 	if len(args) < 2 {
 		return shim.Error("Expecting more args")
 	}
 	Veh := NewVehicle("Truck", args[1])
-	orders := []FuelOrderID(args[2:])
-	//check that client supplied properly the # of args
+	orders := args[2:]
 	if len(orders) == 0 {
 		return shim.Error("At least one delivery should be specified")
 	} else if len(orders)%4 != 0 {
-		return shim.Error("Arguments dont match!Pattern should be {FuelOrderID,EstTime,Sloc,Dest}... ")
+		return shim.Error(fmt.Sprintf("Arguments dont match!Pattern should be {FuelOrderID,EstTime,Sloc,Dest}... Instead args are %d", len(orders)))
 	}
 	Plan := make(map[FuelOrderID]DeliveryDetails)
-	//orders[i] = orderID , orders[i+1] = estTime , i+2 = sloc , i+3 = dest
+	//orders[i] = FuelorderID , orders[i+1] = estTime , i+2 = sloc , i+3 = dest
 	for i := 0; i < len(orders); i += 4 {
-		fuelOrderbytes, _ := stub.GetState(orders[i])
+		var id FuelOrderID = orders[i]
+		fuelOrderbytes, _ := stub.GetState(id)
 		if fuelOrderbytes == nil {
-			return shim.Error(fmt.Sprintf("FuelOrderID %s does not exist", orders[i]))
-		} else {
-			fuelOrder := FuelOrder{}
-			json.Unmarshal(fuelOrderbytes, &fuelOrder)
-			fuelOrder.AD.State = "ON_WAY"
-			newFuelOrderbytes, _ := json.Marshal(fuelOrder)
-			err := stub.PutState(orders[i], newFuelOrderbytes)
-			if err != nil {
-				return shim.Error(fmt.Sprint("Failed to add %s with different state", orders[i]))
-			}
+			return shim.Error(fmt.Sprintf("FuelOrderID %s does not exist", id))
+		}
+		fuelOrder := FuelOrder{}
+		json.Unmarshal(fuelOrderbytes, &fuelOrder)
+		fuelOrder.AD.State = "ON_WAY"
+		newFuelOrderbytes, _ := json.Marshal(fuelOrder)
+		err := stub.PutState(id, newFuelOrderbytes)
+		if err != nil {
+			return shim.Error(fmt.Sprint("Failed to add %s with different state", id))
 
 		}
 		DD, err := NewDeliveryDetails(orders[i+1], orders[i+2], orders[i+3])
 		if err != nil {
 			return shim.Error(err.Error())
 		}
-		Plan[orders[i]] = DD
+		Plan[id] = DD
 	}
 
 	fuelDeliveryPlan := FuelDeliveryPlan{Veh, Plan}
@@ -359,6 +380,238 @@ func (s *SmartContract) deliverFuel(stub shim.ChaincodeStubInterface, args []str
 
 	return shim.Success(nil)
 
+}
+
+/*
+func transfer(stub shim.ChaincodeStubInterface, Deliverable d, own, stime, id string) error {
+	switch d.(type) {
+	case Crude:
+		crudeAsBytes, _ := stub.GetState(id)
+		if crudeAsBytes == nil {
+			return shim.Error("Could not locate Crude")
+		}
+		json.Unmarshal(crudeAsBytes,&
+		fmt.Println("")
+	case FuelOrder:
+	}
+
+}
+*/
+
+/*
+if we want to transfer FuelOrder then we should supply {FuelOrderID,owner,curtime,PlanID}
+if we want to transfer Crude then we should supply {Crude,owner,curtime}
+
+Transportation orgs get paid based on the quantity of fuel or crude oil they are delivering.
+
+*/
+func (s *SmartContract) transfer(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+	if len(args) != 4 && len(args) != 3 {
+		return shim.Error("Wrong # of arguments.")
+	}
+	if ok := HasPrefixOrg(args[1]); ok == false {
+		return shim.Error("Owner is not an org")
+	}
+	Timestamp, err := RFCtoTime(args[2])
+	if err != nil {
+		return shim.Error("Timestamp not in RFC3339 format.")
+	}
+	assetAsBytes, _ := stub.GetState(args[0])
+	if assetAsBytes == nil {
+		return shim.Error("Could not locate Asset")
+	}
+	switch id := args[0]; {
+	case strings.HasPrefix(id, "Crude"):
+		crude := Crude{}
+		json.Unmarshal(assetAsBytes, &crude)
+		if crude.AD.State != "ON_WAY" {
+			return shim.Error("Cannot transfer crude if it's state is not ON_WAY")
+		}
+		crude.AD.State = "DELIVERED"
+		crude.DD.Delay = Timestamp.Sub(crude.DD.EstTime).Seconds()
+		//add transfer of money
+		//TODO:create a new func for this
+		var timePenalty float64
+		timePenalty = crude.DD.Delay / 100.0
+		if timePenalty < 0 {
+			timePenalty = 0
+		}
+		shipperPayment := float64(crude.AD.Quantity)/10.0 - timePenalty
+		drillerPayment := crude.AD.Value
+		org1AccBytes, err := stub.GetState("org1")
+		if err != nil {
+			return shim.Error("Please call initLedger before transfer")
+		}
+		org2AccBytes, err = stub.GetState("org2")
+		if err != nil {
+			return shim.Error("Please call initLedger before transfer")
+		}
+		org3AccBytes, err = stub.GetState("org3")
+		if err != nil {
+			return shim.Error("Please call initLedger before transfer")
+		}
+		var org1Amount, org2Amount, org3Amount float64
+		json.Unmarshal(org1AccBytes, &org1Amount)
+		json.Unmarshal(org2AccBytes, &org2Amount)
+		json.Unmarshal(org3AccBytes, &org3Amount)
+		org1Amount += drillerPayment
+		org2Amount += shipperPayment
+		org3Amount -= shipperPayment + drillerPayment
+		//update db (put state)
+		//....
+
+		//end transfer money
+		crude.AD.Owner = args[1]
+		assetAsBytes, _ = json.Marshal(crude)
+		err := stub.PutState(id, assetAsBytes)
+		if err != nil {
+			return shim.Error(fmt.Sprintf("Failed to put %s in db", id))
+		}
+	//change state of fuel and compute delay in deliveryPlan struct
+	case strings.HasPrefix(id, "FuelOrder"):
+		fuelOrder := FuelOrder{}
+		json.Unmarshal(assetAsBytes, &fuelOrder)
+		fuelOrder.AD.Owner = args[1]
+		if fuelOrder.AD.State != "ON_WAY" {
+			return shim.Error("Cannot deliver fuel if it's state is not ON_WAY")
+		}
+		fuelOrder.AD.State = "DELIVERED"
+		if strings.HasPrefix(args[3], "Plan") == false {
+			return shim.Error("PlanID is not of the form 'PlanXXX'")
+		}
+		dplanAsBytes, _ := stub.GetState(args[3])
+		if dplanAsBytes == nil {
+			return shim.Error("Could not locate Plan")
+		}
+		dplan := FuelDeliveryPlan{}
+		json.Unmarshal(dplanAsBytes, &dplan)
+		dd, ok := dplan.Plan[id]
+		if ok == false {
+			return shim.Error("FuelOrderID didn't exist in any plan")
+		}
+		dd.Delay = Timestamp.Sub(dd.EstTime).Seconds()
+		dplan.Plan[id] = dd
+		dplanAsBytes, _ = json.Marshal(dplan)
+		err := stub.PutState(args[3], dplanAsBytes)
+		if err != nil {
+			return shim.Error(fmt.Sprintf("Failed to put %s in db", args[3]))
+		}
+		assetAsBytes, _ = json.Marshal(fuelOrder)
+		err = stub.PutState(id, assetAsBytes)
+		if err != nil {
+			return shim.Error(fmt.Sprintf("Failed to put %s in db", id))
+		}
+	default:
+		return shim.Error("Either this is not a valid ID or it's not deliverable")
+	}
+	return shim.Success(nil)
+}
+
+func (s *SmartContract) queryAsset(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorect # of args")
+	}
+	assetAsBytes, _ := stub.GetState(args[0])
+	if assetAsBytes == nil {
+		return shim.Error("Could not locate asset")
+	}
+	return shim.Success(assetAsBytes)
+}
+
+func (s *SmartContract) queryAssetByRange(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+	var startKey, endKey string
+	if len(args) != 1 {
+		return shim.Error("Expecting 1 arg")
+	}
+	switch id := args[0]; id {
+	case "Crude":
+	case "Fuel":
+	case "FuelOrder":
+	case "Plan":
+	default:
+		return shim.Error("Arg should be one of {Crude,Fuel,FuelOrder,Plan}")
+	}
+	startKey = args[0] + "0"
+	endKey = args[0] + "999"
+
+	resultsIterator, err := stub.GetStateByRange(startKey, endKey)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add comma before array members,suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+	fmt.Printf("- query:\n%s\n", buffer.String())
+	return shim.Success(buffer.Bytes())
+}
+
+/*
+Create accounts for each organization.
+Form of accounts : key=org_name (e.g 'org1') and value=100000 (arbitrary starting amount)
+An adversary can call initLedger multiple times in order to eliminate his debt,
+so we make a check before proceeding into actions.
+*/
+func (s *SmartContract) initLedger(stub shim.ChaincodeStubInterface, args []string) sc.Response {
+	if bytes, _ := stub.GetState("org1"); bytes != nil {
+		return shim.Error("initLedger has been called already and should be called only once!")
+	}
+	jbytes, _ := json.Marshal(100000.0)
+	err := stub.PutState("org1", jbytes)
+	if err != nil {
+		return shim.Error("Failed to create account for org1")
+	}
+	err = stub.PutState("org2", jbytes)
+	if err != nil {
+		return shim.Error("Failed to create account for org2")
+	}
+	err = stub.PutState("org3", jbytes)
+	if err != nil {
+		return shim.Error("Failed to create account for org3")
+	}
+	err = stub.PutState("org4", jbytes)
+	if err != nil {
+		return shim.Error("Failed to create account for org4")
+	}
+	err = stub.PutState("org5", jbytes)
+	if err != nil {
+		return shim.Error("Failed to create account for org5")
+	}
+	err = stub.PutState("org6", jbytes)
+	if err != nil {
+		return shim.Error("Failed to create account for org6")
+	}
+	return shim.Success(nil)
+}
+
+func main() {
+
+	// Create a new Smart Contract
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
 }
 
 /*
@@ -515,11 +768,3 @@ func (s *SmartContract) changeDeliveryTime(APIstub shim.ChaincodeStubInterface, 
 calls the Start function
 The main function starts the chaincode in the container during instantiation.
 */
-func main() {
-
-	// Create a new Smart Contract
-	err := shim.Start(new(SmartContract))
-	if err != nil {
-		fmt.Printf("Error creating new Smart Contract: %s", err)
-	}
-}
